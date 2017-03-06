394752d3793c9f30e009abba2524552fa030e781
Víctor Cabezas <wiston666@gmail.com>
2017-03-06 23:08:19 +0100

    YellowCross done

--- Solver/Beginner/YellowCrossSolver.py
+++ Solver/Beginner/YellowCrossSolver.py
@@ -13,23 +13,22 @@ class YellowCrossSolver(Solver):
     def solution(self):
         solution = []
         # Apply F R U R' U' F' once, twice or thrice
-        up_yellows = [edge for edge in ['FU', 'RU', 'LU', 'BU'] if self.cube.cubies[edge].color_facing('Y')]
-        
-        if len(up_yellows) == 1:
+        up_yellows = [edge for edge in ['FU', 'RU', 'LU', 'BU'] if self.cube.cubies[edge].color_facing('Y') == 'U']
+        if len(up_yellows) == 0:
             self.apply_algorithm(solution)
-            self.move("U2")
+            self.move("U2", solution)
             self.apply_algorithm(solution)
             self.apply_algorithm(solution)
         elif len(up_yellows) == 2:
             # If not line position, it's L position
-            if ('FU' in up_yellows and 'BU' in up_yellows) or ('RU' in up_yellows and 'LU' in up_yellows):
+            if not ('FU' in up_yellows and 'BU' in up_yellows) or not ('RU' in up_yellows and 'LU' in up_yellows):
                 # Rotate until L is at BU - LU
-                while not self.cube.cubies['BU'].color_facing('Y') or not self.cube.cubies['LU'].color_facing('Y'):
-                    self.move("Y", solution)
+                while not (self.cube.cubies['BU'].color_facing('Y') == 'U' and self.cube.cubies['LU'].color_facing('Y') == 'U'):
+                    self.move("U", solution)
                 self.apply_algorithm(solution)
-             # Rotate until line is at RU - LU
-            while not self.cube.cubies['RU'].color_facing('Y') or not self.cube.cubies['LU'].color_facing('Y'):
-                self.move("Y", solution)   
+            # Rotate until line is at RU - LU
+            while not (self.cube.cubies['RU'].color_facing('Y') == 'U' and self.cube.cubies['LU'].color_facing('Y') == 'U'):
+                self.move("U", solution)
             # Line position
             self.apply_algorithm(solution)
 
--- Solver/Beginner/__init__.py
+++ Solver/Beginner/__init__.py
@@ -3,6 +3,7 @@ from .. import Solver
 from . import WhiteCrossSolver
 from . import WhiteFaceSolver
 from . import SecondLayerSolver
+from . import YellowCrossSolver
 
 class BeginnerSolver(Solver):
     def solution(self):
@@ -10,4 +11,5 @@ class BeginnerSolver(Solver):
         solution = WhiteCrossSolver.WhiteCrossSolver(cube).solution()
         solution += WhiteFaceSolver.WhiteFaceSolver(cube).solution()
         solution += SecondLayerSolver.SecondLayerSolver(cube).solution()
+        solution += YellowCrossSolver.YellowCrossSolver(cube).solution()
         return solution
