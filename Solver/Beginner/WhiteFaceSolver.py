79a63eb002b3a13a6dc03ecf5f5fde80d2458dd1
Víctor Cabezas <wiston666@gmail.com>
2017-03-06 20:56:32 +0100

    WhiteFaceSolver done

--- Solver/Beginner/WhiteFaceSolver.py
+++ Solver/Beginner/WhiteFaceSolver.py
@@ -11,9 +11,86 @@ class WhiteFaceSolver(Solver):
             right_color = self.cube.cubies['R'].facings['R']
 
             goal_cubie = self.cube.search_by_colors('W', front_color, right_color)
-            print "Colors (F)", front_color, "(R)", right_color
-            print "Goal cubie is at", goal_cubie
+            step_solution = []
+            goal_cubie_obj = self.cube.cubies[goal_cubie]
+            if goal_cubie == 'DFR':
+                if goal_cubie_obj.color_facing('W') == 'F':
+                    step_solution.append("R")
+                    step_solution.append("U'")
+                    step_solution.append("R'")
+                elif goal_cubie_obj.color_facing('W') == 'R':
+                    step_solution.append("R")
+                    step_solution.append("U")
+                    step_solution.append("R'")
+                    step_solution.append("U'")
+            elif goal_cubie == 'DFL':
+                if goal_cubie_obj.color_facing('W') == 'F':
+                    step_solution.append("L'")
+                    step_solution.append("U")
+                    step_solution.append("L")
+                    step_solution.append("U'")
+                elif goal_cubie_obj.color_facing('W') in ['L', 'D']:
+                    step_solution.append("L'")
+                    step_solution.append("U'")
+                    step_solution.append("L")
+            elif goal_cubie == 'BDL':
+                if goal_cubie_obj.color_facing('W') in ['B', 'D']:
+                    step_solution.append("B'")
+                    step_solution.append("U2")
+                    step_solution.append("B")
+                elif goal_cubie_obj.color_facing('W') == 'L':
+                    step_solution.append("B'")
+                    step_solution.append("U")
+                    step_solution.append("B")
+                    step_solution.append("U2")
+            elif goal_cubie == 'BDR':
+                if goal_cubie_obj.color_facing('W') in ['B', 'D']:
+                    step_solution.append("B")
+                    step_solution.append("U")
+                    step_solution.append("B'")
+                elif goal_cubie_obj.color_facing('W') == 'R':
+                    step_solution.append("B")
+                    step_solution.append("U'")
+                    step_solution.append("B'")
+                    step_solution.append("U")
+            else:
+                # Cubie is in upper face, place it on FRU
+                if goal_cubie == 'BRU':
+                    step_solution.append("U")
+                elif goal_cubie == 'BLU':
+                    step_solution.append("U2")
+                elif goal_cubie == 'FLU':
+                    step_solution.append("U'")
+                # else is already at FRU
+            
+            for m in step_solution:
+                self.cube.move(Move(m))
+            # Cubie is at FRU, place it at DRU with correct orientation
+            solution.extend(step_solution)
+            step_solution = []
 
+            if self.cube.cubies['FRU'].color_facing('W') == 'F':
+                step_solution.append("F'")
+                step_solution.append("U'")
+                step_solution.append("F")
+            elif self.cube.cubies['FRU'].color_facing('W') == 'R':
+                step_solution.append("R")
+                step_solution.append("U")
+                step_solution.append("R'")
+            elif self.cube.cubies['FRU'].color_facing('W') == 'U':
+                step_solution.append("R")
+                step_solution.append("U2")
+                step_solution.append("R'")
+                step_solution.append("U'")
+                step_solution.append("R")
+                step_solution.append("U")
+                step_solution.append("R'")
+            
+            for m in step_solution:
+                self.cube.move(Move(m))
+            solution.extend(step_solution)
+            # Cubie is placed, move to next
+            
             solution.append('Y')
             self.cube.move(Move('Y'))
         return solution
