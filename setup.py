f33ad0e431678ba72a0c5008306f481eff4a5930
Víctor Cabezas <wiston666@gmail.com>
2017-03-20 21:14:01 +0100

    Add README.md, update setup.py

---
+++ setup.py
@@ -0,0 +1,50 @@
+from pip.req import parse_requirements
+from setuptools import setup, find_packages
+from os import path
+from codecs import open
+
+__author__ = 'Victor Cabezas'
+
+here = path.abspath(path.dirname(__file__))
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+install_reqs = [str(r.req) for r in parse_requirements('requirements.txt', session='hack')]
+
+setup(
+    name='rubik_solver',
+    version='0.0.1',
+    description='Rubik solver algorithms',
+    long_description=long_description,
+    url='',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=install_reqs,
+    scripts=['rubik.py']
+)
