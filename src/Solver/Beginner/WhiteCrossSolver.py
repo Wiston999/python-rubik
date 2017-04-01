38c09fa181e1cbaa181d869799b448728dbabf0b
Víctor Cabezas <wiston666@gmail.com>
2017-04-01 19:40:52 +0200

    Add test_BeginnerSolver, improve WhiteCrossSolver to be more testable

--- src/Solver/Beginner/WhiteCrossSolver.py
+++ src/Solver/Beginner/WhiteCrossSolver.py
@@ -6,64 +6,65 @@ class WhiteCrossSolver(Solver):
     '''
     This class solves the white cross on the down face
     '''
+    @staticmethod
+    def first_step(white_facing, color_facing):
+        if white_facing == 'D':
+            solution = "%s2" % color_facing
+        elif white_facing == 'F':
+            if color_facing == 'U':
+                solution = ["F", "R", "U'", "R'", "F'"]
+            elif color_facing == 'D':
+                solution = ["F'", "R", "U'", "R'"]
+            elif color_facing == 'R':
+                solution = ["R", "U", "R'"]
+            elif color_facing == 'L':
+                solution = ["L'", "U'", "L"]
+        elif white_facing == 'B':
+            if color_facing == 'U':
+                solution = ["B", "L", "U'", "L'", "B'"]
+            elif color_facing == 'D':
+                solution = ["B", "R'", "U", "R"]
+            elif color_facing == 'R':
+                solution = ["R'", "U", "R"]
+            elif color_facing == 'L':
+                solution = ["L", "U'", "L'"]
+        elif white_facing == 'L':
+            if color_facing == 'U':
+                solution = ["L", "F", "U'", "F'", "L'"]
+            elif color_facing == 'D':
+                solution = ["L'", "F", "U'", "F'"]
+            elif color_facing == 'F':
+                solution = ["F", "U'", "F'"]
+            elif color_facing == 'B':
+                solution = ["B'", "U", "B"]
+        elif white_facing == 'R':
+            if color_facing == 'U':
+                solution = ["R'", "F'", "U", "F", "R"]
+            elif color_facing == 'D':
+                solution = ["R", "F'", "U", "F"]
+            elif color_facing == 'F':
+                solution = ["F'", "U", "F"]
+            elif color_facing == 'B':
+                solution = ["B", "U'", "B'"]
+        return solution
+
+        
     def solution(self):
         solution = []
         for color in 'RGOB':
             cubie_position = self.cube.search_by_colors('W', color)
-            step_solution = []
+            
             orig_cubie = self.cube.cubies[cubie_position]
             white_facing = orig_cubie.color_facing('W')
             color_facing = orig_cubie.color_facing(color)
-
+            step_solution = WhiteCrossSolver.first_step(white_facing, color_facing)
             # First goal is to put white sticker on top face
-            if white_facing == 'D':
-                step_solution.append("%s2" % color_facing)
-            elif white_facing == 'F':
-                if color_facing == 'U':
-                    step_solution.extend(["F", "R", "U'", "R'", "F'"])
-                elif color_facing == 'D':
-                    step_solution.extend(["F'", "R", "U'", "R'"])
-                elif color_facing == 'R':
-                    step_solution.extend(["R", "U", "R'"])
-                elif color_facing == 'L':
-                    step_solution.extend(["L'", "U'", "L"])
-            elif white_facing == 'B':
-                if color_facing == 'U':
-                    step_solution.extend(["B", "L", "U'", "L'", "B'"])
-                elif color_facing == 'D':
-                    step_solution.extend(["B", "R'", "U", "R"])
-                elif color_facing == 'R':
-                    step_solution.extend(["R'", "U", "R"])
-                elif color_facing == 'L':
-                    step_solution.extend(["L", "U'", "L'"])
-            elif white_facing == 'L':
-                if color_facing == 'U':
-                    step_solution.extend(["L", "F", "U'", "F'", "L'"])
-                elif color_facing == 'D':
-                    step_solution.extend(["L'", "F", "U'", "F'"])
-                elif color_facing == 'F':
-                    step_solution.extend(["F", "U'", "F'"])
-                elif color_facing == 'B':
-                    step_solution.extend(["B'", "U", "B"])
-            elif white_facing == 'R':
-                if color_facing == 'U':
-                    step_solution.extend(["R'", "F'", "U", "F", "R"])
-                elif color_facing == 'D':
-                    step_solution.extend(["R", "F'", "U", "F"])
-                elif color_facing == 'F':
-                    step_solution.extend(["F'", "U", "F"])
-                elif color_facing == 'B':
-                    step_solution.extend(["B", "U'", "B'"])
-                    step_solution.append("B")
-                    step_solution.append("U'")
-                    step_solution.append("B'")
 
             for m in step_solution:
                 self.cube.move(Move(m))
             solution.extend(step_solution)
 
             # Second goal is to place the cubie on the top over its place
-            print "Placing on top", color
             while self.cube.cubies['FU'].facings['U'] != 'W' or self.cube.cubies['FU'].facings['F'] != color:
                 solution.append('U')
                 self.cube.move(Move('U'))
---
+++ tests/test_BeginnerSolver.py
@@ -0,0 +1,51 @@
+from src.Move import Move
+from src.Cubie import Cube
+from src.Solver import Beginner
+from src.Solver.Beginner import WhiteCrossSolver
+import unittest
+
+class TestWhiteCrossSolver(unittest.TestCase):
+    def test_solution(self):
+        cases = [
+            ('D', 'F'),
+            ('D', 'B'),
+            ('D', 'R'),
+            ('D', 'L'),
+            ('F', 'U'),
+            ('F', 'D'),
+            ('F', 'R'),
+            ('F', 'L'),
+            ('B', 'U'),
+            ('B', 'D'),
+            ('B', 'R'),
+            ('B', 'L'),
+            ('R', 'U'),
+            ('R', 'D'),
+            ('R', 'F'),
+            ('R', 'B'),
+            ('L', 'U'),
+            ('L', 'D'),
+            ('L', 'F'),
+            ('L', 'B'),
+        ]
+
+        for c0, c1 in cases:
+            c = Cube()
+            c.cubies[c._t_key(c0+c1)].facings[c0].color = 'W'
+            c.cubies[c._t_key(c0+c1)].facings[c1].color = 'Y'
+
+            steps = WhiteCrossSolver.first_step(c0, c1)
+            for step in steps:
+                c.move(Move(step))
+
+            place = c.search_by_colors('W', 'Y')
+
+            self.assertEqual(c.cubies[place].facings['U'], 'W')
+            # Weird
+            self.assertEqual(c.cubies[place].facings[place.replace('U', '')], 'Y')
+
+
+class TestBeginnerSolver(unittest.TestCase):
+    def test_solution(self):
+        pass
+
