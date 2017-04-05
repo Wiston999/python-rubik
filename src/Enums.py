54caef44272932339b8a7f4823a3f56760ece3b6
Víctor Cabezas <wiston666@gmail.com>
2017-04-05 20:18:22 +0200

    Change iter for items

--- src/Enums.py
+++ src/Enums.py
@@ -1,7 +1,7 @@
 
 def enum(*sequential, **named):
     enums = dict(zip(sequential, range(len(sequential))), **named)
-    reverse = dict((value, key) for key, value in enums.iter())
+    reverse = dict((value, key) for key, value in enums.items())
     enums['reverse_mapping'] = reverse
     return type('Enum', (), enums)
 
