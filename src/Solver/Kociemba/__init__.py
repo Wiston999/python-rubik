a021b66b421dd48d4c32742b5f4f2ed558629f46
Víctor Cabezas <wiston666@gmail.com>
2017-04-26 20:29:58 +0200

    Increase kociemba maxSteps

--- src/Solver/Kociemba/__init__.py
+++ src/Solver/Kociemba/__init__.py
@@ -4,7 +4,7 @@ from . import Search
 
 
 class KociembaSolver(Solver):
-    def solution(self, maxDepth=21, timeOut=100):
+    def solution(self, maxDepth=23, timeOut=100):
         solution = Search.Search.solution(
             self.cube.to_naive_cube().to_face_cube().to_String(),
             maxDepth,
