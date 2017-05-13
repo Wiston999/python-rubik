cc3a19bd46d40a177dbb906749ae7de4f9f025d5
Víctor Cabezas <wiston666@gmail.com>
2017-05-13 19:55:27 +0200

    Disable guzzle extension

--- docs/conf.py
+++ docs/conf.py
@@ -88,8 +88,6 @@ todo_include_todos = True
 #
 html_theme = 'guzzle_sphinx_theme'
 
-extensions.append("guzzle_sphinx_theme")
-
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
