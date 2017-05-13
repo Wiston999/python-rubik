74efeb314403819a6242b741b7e3a91647d3b131
Víctor Cabezas <wiston666@gmail.com>
2017-05-14 00:27:09 +0200

    Disable guzzle extension

--- docs/conf.py
+++ docs/conf.py
@@ -86,15 +86,15 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'guzzle_sphinx_theme'
+# html_theme = 'guzzle_sphinx_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {
-    "project_nav_name": "Rubik Solver",
-}
+# html_theme_options = {
+#     "project_nav_name": "Rubik Solver",
+# }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
